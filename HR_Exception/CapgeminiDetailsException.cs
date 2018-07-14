b7aa29c10836e82cbdaf9ca5b641ca86a7a706d7
bsukumar <bsukumar@CHNSIPDT0T829>
2018-07-14 11:09:41 +0530

    1st Commit

--- HR_Exception/CapgeminiDetailsException.cs
+++ HR_Exception/CapgeminiDetailsException.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// 
+/// </summary>
 namespace HR_Exception
 {
    public class CapgeminiDetailsException:Exception
